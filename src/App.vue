05a44e957e131628ddfd6046487cc35b3825d45f
Mark <junheak65@xingsoft.co.kr>
2023-12-04 13:05:09 +0900

    reactive 함수의 형 설정

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import {reactive, Ref, ref, UnwrapRef} from "vue";
+
+const count: Ref<number> = ref(0);
+
+interface User {
+  firstName: string,
+  lastName: string,
+  age: number
+}
+
+const user: User = reactive({} as User)
+
+user.firstName = 'jun'
+user.lastName = 'park'
+user.age = 23
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <body>
+    <div>Count: {{ count }}</div>
+  <button @click="count++">add Count</button>
+
+  <div>FullName {{`${user.firstName} ${user.lastName}`}}</div>
+  </body>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
